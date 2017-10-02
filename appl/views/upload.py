21d6b20801f7c7799d71bd90ed3b2ed4a92681ea
M.Charattrakool <m.charattrakool@gmail.com>
2017-10-02 18:12:22 +0700

    check file size

--- appl/views/upload.py
+++ appl/views/upload.py
@@ -25,17 +25,17 @@ def upload(request, document_id):
                                                   pk=document_id)
     if request.method != 'POST':
         return HttpResponseForbidden()
-
+    size_limit = project_uploaded_document.size_limit
     form = UploadedDocumentForm(request.POST, request.FILES)
     if form.is_valid():
+        print(size_limit, form.cleaned_data['uploaded_file'].size)
         if not project_uploaded_document.can_have_multiple_files:
             old_uploaded_documents = project_uploaded_document.get_uploaded_documents_for_applicant(applicant)
             for odoc in old_uploaded_documents:
                 odoc.uploaded_file.delete()
                 odoc.delete()
-        
-        uploaded_document = form.save(commit=False)
 
+        uploaded_document = form.save(commit=False)
         uploaded_document.applicant = request.applicant
         uploaded_document.project_uploaded_document = project_uploaded_document
         uploaded_document.admission_project = project_uploaded_document.admission_project
@@ -49,12 +49,11 @@ def upload(request, document_id):
 
         project_uploaded_document.form = upload_form_for(project_uploaded_document)
         project_uploaded_document.applicant_uploaded_documents = project_uploaded_document.get_uploaded_documents_for_applicant(applicant)
-        
+
         result = {'result': 'OK',
                   'html': template.render({ 'project_uploaded_document': project_uploaded_document },
                                           request) }
     else:
-        result = {'result': 'ERROR'}        
+        result = {'result': 'ERROR'}
     return HttpResponse(json.dumps(result),
                         content_type='application/json')
-
