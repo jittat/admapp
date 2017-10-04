c05662bdf6f3ba5e0fbf4dab23b46ffe83c1d3a1
M.Charattrakool <m.charattrakool@gmail.com>
2017-10-04 18:11:14 +0700

    create document view

--- appl/urls.py
+++ appl/urls.py
@@ -4,6 +4,7 @@ from appl import views
 from appl.views import upload as upload_views
 from appl.views import general_forms
 from appl.views import major_selection
+from appl.views import media_serve as media
 
 app_name = 'appl'
 urlpatterns = [
@@ -19,5 +20,5 @@ urlpatterns = [
 
     url(r'^payment/(\d+)/$', views.payment, name='payment'),
     url(r'^payment/(\d+)/barcode/(\d+)\.png$', views.payment_barcode, name='payment-barcode'),
+    url(r'^media/$', media.document_view, name='document-view')
 ]
-
--- appl/views/media_serve.py
+++ appl/views/media_serve.py
@@ -1,1 +1,5 @@
-        
+from django.http import HttpResponseForbidden
+
+
+def document_view(request,document_id):
+    return HttpResponseForbidden()
