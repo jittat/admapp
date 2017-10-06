30f7f708a0b6a49a01043c2c35240b3d39f0954f
M.Charattrakool <m.charattrakool@gmail.com>
2017-10-06 13:06:12 +0700

    move validate to new function

--- appl/views/media_serve.py
+++ appl/views/media_serve.py
@@ -2,18 +2,29 @@ from django.conf import settings
 from django.http import HttpResponseForbidden, HttpResponse, HttpResponseNotFound
 from appl.models import UploadedDocument,Applicant,ProjectUploadedDocument
 from django.core.exceptions import ObjectDoesNotExist
+from regis.decorators import appl_login_required
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+
 import os
 
-def document_view(request,document_id=0, applicant_id=0, admission_project_id=0):
-    try:
-        applicant = Applicant.objects.get(pk=applicant_id)
-        project_uploaded_document = ProjectUploadedDocument.objects.get(pk=admission_project_id)
-        uploaded_document = UploadedDocument.objects.get(pk=document_id)
-    except ObjectDoesNotExist:
-        return HttpResponseNotFound()
+def get_uploaded_documents_or_403(request, document_id, applicant_id, admission_project_id):
 
-    if uploaded_document.applicant != applicant or uploaded_document.project_uploaded_document != project_uploaded_document:
-        return HttpResponseForbidden()
+    applicant = get_object_or_404(Applicant, pk=applicant_id)
+    project_uploaded_document = get_object_or_404(ProjectUploadedDocument,pk=admission_project_id)
+    uploaded_document = get_object_or_404(UploadedDocument,pk=document_id)
+
+    if uploaded_document.applicant != applicant \
+    or uploaded_document.project_uploaded_document != project_uploaded_document \
+    or uploaded_document.applicant != request.applicant:
+        raise PermissionDenied
+
+    return uploaded_document
+
+@appl_login_required
+def document_view(request,document_id=0, applicant_id=0, admission_project_id=0):
+
+    uploaded_document = get_uploaded_documents_or_403(request, document_id, applicant_id, admission_project_id)
 
     doc_file = uploaded_document.uploaded_file
 
