82137e7ad644d3a45c26c1b6a8a96f868237bf81
Jittat Fakcharoenphol <jittat@gmail.com>
2018-04-03 13:14:17 +0700

    imports clearing results

---
+++ scripts/import_clearing_results.py
@@ -0,0 +1,51 @@
+from django_bootstrap import bootstrap
+bootstrap()
+
+import sys
+import csv
+from datetime import datetime
+
+from regis.models import Applicant
+from appl.models import AdmissionProject, AdmissionResult, AdmissionRound, ProjectApplication
+
+def read_applicant_numbers(filename):
+    return [int(st.strip()) for st in open(filename).readlines()]
+
+def main():
+    project_id = sys.argv[1]
+    round_id = sys.argv[2]
+    filename = sys.argv[3]
+
+    admission_project = AdmissionProject.objects.get(pk=project_id)
+    admission_round = AdmissionRound.objects.get(pk=round_id)
+
+    confirmed_applicant_numbers = read_applicant_numbers(filename)
+
+    accepted_results = AdmissionResult.objects.filter(admission_project=admission_project,
+                                                      admission_round=admission_round,
+                                                      is_accepted=True).all()
+
+    accepted_applications = dict([(r.application.get_number(),r.application) for r in accepted_results])
+
+    for number in confirmed_applicant_numbers:
+        if number not in accepted_applications:
+            print('ERROR ' + number)
+            continue
+
+        application = accepted_applications[number]
+        applicant = application.applicant
+        applicant.confirmed_application = application
+        applicant.save()
+
+        results = application.admissionresult_set.all()
+        if len(results) != 1:
+            print('ERROR result')
+            continue
+        result = results[0]
+        result.has_confirmed = True
+        result.save()
+        
+        print(applicant)
+
+if __name__=='__main__':
+    main()
---
+++ scripts/list_projects.py
@@ -0,0 +1,25 @@
+from django_bootstrap import bootstrap
+bootstrap()
+
+import sys
+import csv
+from datetime import datetime
+
+from regis.models import Applicant
+from appl.models import AdmissionProject, AdmissionRound, AdmissionProjectRound
+
+def read_applicant_numbers(filename):
+    return [int(st.strip()) for st in open(filename).readlines()]
+
+def main():
+    admission_round_id = sys.argv[1]
+    admission_round = AdmissionRound.objects.get(pk=admission_round_id)
+
+    projects = admission_round.admissionprojectround_set.all()
+    for p in projects:
+        if p.admission_project.is_available:
+            print(p.admission_project.id, p.admission_project)
+
+
+if __name__ == '__main__':
+    main()
