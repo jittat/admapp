5a068a59b68b8ea85a6a51af69d43e8bbe534836
Jittat Fakcharoenphol <jittat@gmail.com>
2018-05-23 02:20:00 +0700

    imports applicants from round 3

---
+++ scripts/import_tcas3_applicants.py
@@ -0,0 +1,98 @@
+from django_bootstrap import bootstrap
+bootstrap()
+
+import sys
+import csv
+from random import choice
+from datetime import datetime
+
+from regis.models import Applicant
+from appl.models import Major, MajorSelection, ProjectApplication, AdmissionProjectRound
+
+def random_email():
+    return ''.join([choice('abcdefghijklmnopqrstuvwxyz') for i in range(10)]) + '@ku.ac.th'
+
+
+def read_applicants(filename):
+    applicants = []
+    with open(filename) as f:
+        reader = csv.reader(f)
+        for items in reader:
+            a = {
+                'natid': items[0],
+                'prefix': items[1],
+                'first_name': items[2],
+                'last_name': items[3]
+            }
+            a['majors'] = [int(x) for x in items[5:]]
+            applicants.append(a)
+            
+    return applicants
+
+def main():
+    applicant_filename = sys.argv[1]
+    project_round_id = sys.argv[2]
+
+    project_round = AdmissionProjectRound.objects.get(pk=project_round_id)
+    project = project_round.admission_project
+    admission_round = project_round.admission_round
+    
+    applicants = read_applicants(applicant_filename)
+
+    now = datetime.now()
+    
+    majors = dict([(m.number, m) for m in
+                   Major.objects.filter(admission_project=project).all()])
+    
+    check_dup = False
+    
+    for a in applicants:
+        old_apps = Applicant.objects.filter(national_id=a['natid']).all()
+        if (len(old_apps) != 0) and check_dup:
+            print('ERROR dup', a['natid'])
+            continue
+
+        if len(old_apps) != 0:
+            app = old_apps[0]
+        else:
+            app = Applicant()
+
+        app.national_id = a['natid']
+        app.prefix = a['prefix']
+        app.first_name = a['first_name']
+        app.last_name = a['last_name']
+        app.email = random_email()
+        app.random_password()
+
+        app.save()
+
+        if app.project_applications.count() != 0:
+            application = app.project_applications.all()[0]
+        else:
+            application = ProjectApplication()
+
+        application.applicant = app
+        application.admission_project = project
+        application.admission_round = admission_round
+        application.applied_at = now
+        
+        application.save()
+
+        major_selection = application.get_major_selection()
+        if not major_selection:
+            major_selection = MajorSelection()
+
+        major_selection.applicant = app
+        major_selection.project_application = application
+        major_selection.admission_project = project
+        major_selection.admission_round = admission_round
+        major_selection.set_majors([majors[num]
+                                    for num
+                                    in a['majors']])
+        major_selection.save()
+
+        print(a['natid'])
+            
+
+if __name__ == '__main__':
+    main()
