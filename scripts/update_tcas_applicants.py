a4b35a93401ae9b3847bc9829ffc9e37c61bfb02
Jittat Fakcharoenphol <jittat@gmail.com>
2018-06-07 09:04:19 +0700

    updates tcas applicants to real names

---
+++ scripts/update_tcas_applicants.py
@@ -0,0 +1,38 @@
+from django_bootstrap import bootstrap
+bootstrap()
+
+import sys
+import csv
+from datetime import datetime
+
+from regis.models import Applicant
+
+def main():
+    applicant_filename = sys.argv[1]
+
+    counter = 0
+    with open(applicant_filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for items in reader:
+            nat_id = items[0]
+
+            try:
+                a = Applicant.objects.get(national_id=nat_id)
+            except:
+                print('not found', nat_id)
+                continue
+
+            a.national_id = 'T' + items[1]
+            a.prefix = items[2].strip()
+            a.first_name = items[3].strip()
+            a.last_name = items[4].strip()
+            a.save()
+            
+            counter += 1
+
+    print('Updated',counter,'applicants')
+        
+
+if __name__ == '__main__':
+    main()
+    
