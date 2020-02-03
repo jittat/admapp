6a1bfc83ae19dd57a4bff18602c4ae1c148a4b91
Jittat Fakcharoenphol <jittat@gmail.com>
2020-02-03 23:05:35 +0700

    imports confirmation results

---
+++ scripts/import_confirmation_results_tcas63.py
@@ -0,0 +1,78 @@
+from django_bootstrap import bootstrap
+bootstrap()
+
+import sys
+import csv
+from datetime import datetime
+
+from regis.models import Applicant
+from appl.models import AdmissionProject, AdmissionResult, AdmissionRound, ProjectApplication, Major
+from backoffice.models import MajorInterviewCallDecision
+
+from backoffice.views.projects import load_major_applicants
+from backoffice.views.projects import load_check_marks_and_results
+from backoffice.views.projects import sort_applicants_by_calculated_scores
+from backoffice.views.projects import update_interview_call_status
+
+
+def main():
+    round_id = sys.argv[1]
+
+    result_filename = sys.argv[2]
+
+    is_fake = len(sys.argv) <= 3 or (sys.argv[3] != 'real')
+
+    counter = 0
+    with open(result_filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            if row['university_id'] == '':
+                continue
+
+            
+            nat_id = row['citizen_id']
+            passport = row['passport']
+            decision = row['interview_status']
+            program_id = row['program_id']
+
+
+            if nat_id != '0':
+                applicant = Applicant.objects.get(national_id=nat_id)
+            else:
+                applicant = Applicant.objects.get(passport_number=passport)
+
+            if not applicant:
+                print('Applicant not found', nat_id, passport, row['first_name_th'])
+                continue
+            
+            admission_results = AdmissionResult.objects.filter(applicant=applicant).all()
+            accepted_results = [res for res in admission_results if res.is_accepted]
+
+            if len(accepted_results) != 1:
+                print('ERROR accepted results', accepted_results)
+                continue
+
+            admission_result = accepted_results[0]
+            application = admission_result.application
+
+            if decision == '1':
+                admission_result.has_confirmed = True
+                if not is_fake:
+                    admission_result.save()
+                    applicant.confirmed_application = application
+                    applicant.save()
+            else:
+                admission_result.has_confirmed = False
+                if not is_fake:
+                    admission_result.save()
+
+            counter += 1
+            print(counter, admission_result.is_tcas_confirmed, applicant)
+            
+            if counter % 1000 == 0:
+                print(counter)
+
+            
+if __name__ == '__main__':
+    main()
