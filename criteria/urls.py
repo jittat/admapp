1a526f593b570bf5ca084cac59acefe3bb267dfa
Monthol Charattrakool <monthol@cleverse.com>
2020-09-19 20:06:54 +0700

    :construction: create criteria path

--- criteria/urls.py
+++ criteria/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url, include
 from django.urls import path
 
 from . import views
+from criteria.views import criterion
 
 app_name = 'criteria'
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^$', criterion.index, name='criterion-index'),
+    url(r'^(\d+)/(\d+)/add-major$', criterion.create, name='criterion-create'),
+]
