f20e16bbe5568017d904601384c0fd7ac8632a5f
Monthol Charattrakool <monthol@cleverse.com>
2020-09-21 15:30:49 +0700

    :sparkle: create criteria complete process

--- criteria/models.py
+++ criteria/models.py
@@ -8,11 +8,13 @@ from appl.models import Major, Faculty
 class AdmissionCriteria(models.Model):
     admission_project = models.ForeignKey(AdmissionProject,
                                           on_delete=models.CASCADE)
+    faculty = models.ForeignKey(
+        Faculty, on_delete=models.CASCADE, null=True, blank=False)
     version = models.IntegerField(default=1)
 
 
 class ScoreCriteria(models.Model):
-    admission_criteria = models.ForeignKey(Major,
+    admission_criteria = models.ForeignKey(AdmissionCriteria,
                                            on_delete=models.CASCADE)
     version = models.IntegerField(default=1)
     primary_order = models.IntegerField()
@@ -22,7 +24,7 @@ class ScoreCriteria(models.Model):
     value = models.DecimalField(
         decimal_places=4, max_digits=12, null=True, blank=True)
     unit = models.CharField(max_length=30, default="")
-    desscription = models.TextField(default="")
+    description = models.TextField(default="")
 
 
 class MajorCuptCode(models.Model):
@@ -54,3 +56,4 @@ class CurriculumMajorAdmissionCriteria(models.Model):
     admission_criteria = models.ForeignKey(
         AdmissionCriteria, on_delete=models.CASCADE)
     slots = models.IntegerField()
+    version = models.IntegerField(default=1)
--- criteria/views/criterion.py
+++ criteria/views/criterion.py
@@ -1,11 +1,14 @@
 import csv
 import json
+from decimal import Decimal
+
 from datetime import datetime
 from django.core import serializers
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseNotFound
+from django.db import transaction
 
 from regis.models import Applicant, LogItem
 from appl.models import AdmissionProject, AdmissionRound
@@ -15,7 +18,7 @@ from appl.models import ProjectUploadedDocument, UploadedDocument, ExamScoreProv
 from backoffice.views.permissions import can_user_view_project, can_user_view_applicant_in_major, can_user_view_applicants_in_major
 from backoffice.decorators import user_login_required
 
-from criteria.models import CurriculumMajor
+from criteria.models import CurriculumMajor, AdmissionCriteria, ScoreCriteria, CurriculumMajorAdmissionCriteria
 
 
 @user_login_required
@@ -31,27 +34,28 @@ def project_index(request, project_id, round_id):
     if not user.profile.is_admission_admin:
         faculty = user.profile.faculty
         user_major_number = user.profile.major_number
+        admission_criterias = AdmissionCriteria.objects.filter(
+            admission_project_id=project_id, faculty_id=faculty.id)
     else:
         faculty = None
         user_major_number = user.profile.ANY_MAJOR
+        admission_criterias = AdmissionCriteria.objects.filter(
+            admission_project_id=project_id)
 
-    majors = project.major_set.select_related(
-        'faculty').order_by('faculty_id').all()
-
-    if faculty:
-        majors = [m for m in majors if m.faculty_id == faculty.id]
+    admission_criterias
 
     return render(request,
                   'criterion/index.html',
                   {'project': project,
                    'admission_round': admission_round,
                    'faculty': faculty,
-                   'majors': majors,
                    'user_major_number': user_major_number,
                    'any_major': user.profile.ANY_MAJOR,
+                   'admission_criterias': admission_criterias
                    })
 
 
+@user_login_required
 def create(request, project_id, round_id):
     user = request.user
     project = get_object_or_404(AdmissionProject, pk=project_id)
@@ -60,9 +64,6 @@ def create(request, project_id, round_id):
 
     if not can_user_view_project(user, project):
         return redirect(reverse('criteria:index'))
-
-    if request.method == 'POST':
-        return render(request, 'criterion/complete.html', {'project': project, 'admission_round': admission_round})
 
     if not user.profile.is_admission_admin:
         faculty = user.profile.faculty
@@ -75,15 +76,68 @@ def create(request, project_id, round_id):
         admission_project_id=project_id).select_related('cupt_code')
 
     if faculty:
-        majors = [m for m in majors if m.cupt_code.faculty_id == faculty.id]
+        majors = [m for m in majors if m.faculty_id == faculty.id]
+
+    if request.method == 'POST':
+        score_criteria_dict = dict()
+        for key in request.POST:
+            splitted_keys = key.split('_')
+            if splitted_keys[0] == "required" or splitted_keys[0] == "scoring":
+                data_key = splitted_keys[0]+'_'+splitted_keys[1]
+                if data_key not in score_criteria_dict:
+                    order_numbers = splitted_keys[1].split('.')
+                    initial_data = {
+                        "criteria_type": splitted_keys[0],
+                        "primary_order": int(order_numbers[0]),
+                        "secondary_order": 0
+                    }
+                    if len(order_numbers) == 2:
+                        initial_data["secondary_order"] = int(order_numbers[1])
+
+                    score_criteria_dict[data_key] = initial_data
+
+                value = request.POST[key]
+                if value.isnumeric():
+                    value = Decimal(value)
+
+                score_criteria_dict[data_key][splitted_keys[2]
+                                              ] = value
+        with transaction.atomic():
+            admission_criteria = AdmissionCriteria(
+                admission_project=project, version=1, faculty=faculty)
+
+            admission_criteria.save()
+
+            for key in score_criteria_dict:
+                s = score_criteria_dict[key]
+                scoring_criteria = ScoreCriteria(
+                    admission_criteria=admission_criteria,
+                    version=1,
+                    primary_order=s["primary_order"],
+                    secondary_order=s["secondary_order"],
+                    criteria_type=s["criteria_type"],
+                    score_type="OTHER",
+                    value=s["value"],
+                    unit=(s["unit"] if "unit" in s else ""),
+                    description=s["title"])
+                scoring_criteria.save()
+
+            major_criteria = CurriculumMajorAdmissionCriteria(
+                admission_criteria=admission_criteria,
+                curriculum_major=majors[0],
+                slots=1000
+            )
+
+            major_criteria.save()
+
+        return render(request, 'criterion/complete.html', {'project': project, 'admission_round': admission_round})
 
     return render(request,
                   'criterion/create.html',
                   {'project': project,
                    'admission_round': admission_round,
                    'faculty': faculty,
-                   'majors': json.dumps([dict({"id": m.id, "title": ("%s (%s) %s") % (m.cupt_code.title, m.cupt_code.program_type, m.cupt_code.major_title)}) for m in majors]),
-
+                   'majors': json.dumps([dict({"id": m.id, "title": ("%s (%s) %s") % (m.cupt_code.title, m.cupt_code.program_type, m.cupt_code.major_title)}) for m in sorted(majors, key=(lambda m: (m.cupt_code.program_code, m.cupt_code.major_title)))]),
                    'user_major_number': user_major_number,
                    'any_major': user.profile.ANY_MAJOR,
                    })
