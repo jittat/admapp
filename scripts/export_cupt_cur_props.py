3f43242a4da3593752d77bcf370c3086f2da14f1
Jittat Fakcharoenphol <jittat@gmail.com>
2020-12-13 05:35:59 +0700

    added more keywords

--- scripts/export_cupt_cur_props.py
+++ scripts/export_cupt_cur_props.py
@@ -296,8 +296,10 @@ SCORE_TYPE_TAGS = [
     { "score_type": 'MIN_GPA21', 'description': 'ผลการเรียนเฉลี่ยรวมของกลุ่มสาระการเรียนรู้ภาษาไทย'},
     { "score_type": 'MIN_GPA22', 'description': 'คะแนนเฉลี่ยกลุ่มสาระคณิตศาสตร์'},
     { "score_type": 'MIN_GPA22', 'description': 'มีคะแนนเฉลี่ยกลุ่มสาระคณิตศาสตร์'},
+    { "score_type": 'MIN_GPA22', 'description': 'ผลการเรียนกลุ่มสาระการเรียนรู้คณิตศาสตร์'},
     { "score_type": 'MIN_GPA23', 'description': 'คะแนนเฉลี่ยกลุ่มสาระวิทยาศาสตร์'},
     { "score_type": 'MIN_GPA23', 'description': 'มีคะแนนเฉลี่ยกลุ่มสาระวิทยาศาสตร์'},
+    { "score_type": 'MIN_GPA23', 'description': 'ผลการเรียนกลุ่มสาระการเรียนรู้วิทยาศาสตร์'},
     { "score_type": 'MIN_GPA28', 'description': 'มีคะแนนเฉลี่ยกลุ่มสาระภาษาต่างประเทศ'},
     { "score_type": 'MIN_GPA28', 'description': 'ผลการเรียนเฉลี่ยรวมของกลุ่มสาระการเรียนรู้ภาษาต่างประเทศ'},
 ]
