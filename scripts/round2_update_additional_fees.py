a7a6ada9be985ad21851793bfd152218dbcc2d75
Jittat Fakcharoenphol <jittat@gmail.com>
2021-03-04 01:20:43 +0700

    added major fee update script

---
+++ scripts/round2_update_additional_fees.py
@@ -0,0 +1,39 @@
+from django_bootstrap import bootstrap
+bootstrap()
+
+from appl.models import Campus, Faculty, AdmissionProject, Major
+
+def main():
+    project_majors = {
+        13: [
+            (59,'ศศ.บ. สาขาวิชาดนตรีไทย',300),
+            (60,'ศศ.บ. สาขาวิชาดนตรีตะวันตก',300),
+        ],
+        16: [
+            (48,'ศษ.บ. สาขาวิชาพลศึกษา',300),
+        ],
+    }
+
+    for pid in project_majors:
+        project = AdmissionProject.objects.get(pk=pid)
+        for m, title, fee in project_majors[pid]:
+            majors = Major.objects.filter(admission_project=project,
+                                          number=m)
+            if len(majors) != 1:
+                print('ERROR', m, title, majors)
+                continue
+
+            major = majors[0]
+
+            if major.title != title:
+                print('ERROR wrong title', m, title, major.title)
+                continue
+            
+            major.additional_fee_per_major = fee
+            major.save()
+
+            print('DONE',project, major, fee)
+
+if __name__ == '__main__':
+    main()
+
