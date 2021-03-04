a4c11661edb004ce82c9894dfee831004db9b621
Jittat Fakcharoenphol <jittat@gmail.com>
2021-03-04 23:19:38 +0700

    updated sport and cultural projects

--- supplements/models.py
+++ supplements/models.py
@@ -176,7 +176,7 @@ PROJECT_SUPPLEMENTS = {
                                 'supplements.views.forms.nat_sport.process_sport_history_form',
                                 'supplements/backoffice/nat_sport/sport_history.html'),
     ],
-    'โควตานักกีฬาดีเด่นฯ': [
+    'นักกีฬาดีเด่นฯ': [
         ProjectSupplementConfig('gen_sport_type',
                                 'ประเภทกีฬาและระดับ',
                                 True,
@@ -194,7 +194,7 @@ PROJECT_SUPPLEMENTS = {
                                 'supplements.views.forms.gen_sport.process_sport_history_form',
                                 'supplements/backoffice/gen_sport/sport_history.html'),
     ],
-    'โควตาศิลปวัฒนธรรม': [
+    'ศิลปวัฒนธรรม': [
         ProjectSupplementConfig('cultural_type',
                                 'ประเภทศิลปวัฒนธรรม',
                                 True,
