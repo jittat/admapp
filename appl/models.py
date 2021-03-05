75f6b09a6f96a0ba21656e2045b4f069a166a340
Jittat Fakcharoenphol <jittat@gmail.com>
2021-03-06 05:28:51 +0700

    added requirement key to upload doc

--- appl/models.py
+++ appl/models.py
@@ -372,6 +372,11 @@ class ProjectUploadedDocument(models.Model):
     
     document_key = models.CharField(max_length=30,
                                     blank=True)
+
+    requirement_key = models.CharField(max_length=10,
+                                       blank=True,
+                                       default='')
+    
     class Meta:
         ordering = ['rank']
 
