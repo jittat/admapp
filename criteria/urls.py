931f69b4374e08a3dd28f828c264820a09aadc00
Jittat Fakcharoenphol <jittat@gmail.com>
2021-09-23 22:30:21 +0700

    moved default views to __init__.py

--- criteria/urls.py
+++ criteria/urls.py
@@ -2,29 +2,28 @@ from django.conf.urls import url, include
 from django.urls import path
 
 from . import views
-from criteria.views import criterion
 
 app_name = 'criteria'
 
 urlpatterns = [
     path('<int:project_id>/<int:round_id>/',
-         criterion.project_index, name='project-index'),
+         views.project_index, name='project-index'),
 
     path('<int:project_id>/<int:round_id>/create/',
-         criterion.create, name='create'),
+         views.create, name='create'),
     path('<int:project_id>/<int:round_id>/<int:criteria_id>/edit/',
-         criterion.edit, name='edit'),
+         views.edit, name='edit'),
     path('<int:project_id>/<int:round_id>/<int:criteria_id>/delete/',
-         criterion.delete, name='delete'),
+         views.delete, name='delete'),
 
     path('<int:project_id>/<int:round_id>/curriculum-majors/',
-         criterion.select_curriculum_majors, name='curriculum-majors'),
+         views.select_curriculum_majors, name='curriculum-majors'),
     path('<int:project_id>/<int:round_id>/curriculum-majors/toggle/<int:code_id>/<value>/',
-         criterion.select_curriculum_majors, name='curriculum-majors-toggle'),
+         views.select_curriculum_majors, name='curriculum-majors-toggle'),
 
     path('curriculum-majors/',
-         criterion.list_curriculum_majors, name='list-curriculum-majors'),
+         views.list_curriculum_majors, name='list-curriculum-majors'),
 
     path('report/<int:project_id>/<int:round_id>/',
-         criterion.project_report, name='project-report'),
+         views.project_report, name='project-report'),
 ]
