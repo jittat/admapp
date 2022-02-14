4bbe736d73ec8fa4be48d9d7891d6fb99ac98b42
Jittat Fakcharoenphol <jittat@gmail.com>
2022-02-15 05:07:23 +0700

    updates messages

--- scripts/import_projects.py
+++ scripts/import_projects.py
@@ -21,9 +21,10 @@ def main():
 
             pid = int(items[0])
 
+            old_project = None
             try:
                 old_project = AdmissionProject.objects.get(pk=pid)
-                old_project.delete()
+                #old_project.delete()
             except:
                 pass
             
@@ -37,15 +38,25 @@ def main():
             max_num_selections = int(items[next_col_base])
             base_fee = int(items[next_col_base + 1])
             cupt_code = items[next_col_base + 2]
-            
-            project = AdmissionProject(id=pid,
-                                       title=title,
-                                       short_title=short_title,
-                                       short_descriptions=short_descriptions,
-                                       slots=slots,
-                                       max_num_selections=max_num_selections,
-                                       base_fee=base_fee,
-                                       cupt_code=cupt_code)
+
+            if not old_project:
+                project = AdmissionProject(id=pid,
+                                           title=title,
+                                           short_title=short_title,
+                                           short_descriptions=short_descriptions,
+                                           slots=slots,
+                                           max_num_selections=max_num_selections,
+                                           base_fee=base_fee,
+                                           cupt_code=cupt_code)
+            else:
+                project = old_project
+                project.title = title
+                project.short_title = short_title
+                project.short_descriptions = short_descriptions
+                project.slots = slots
+                project.max_num_selections = max_num_selections
+                project.base_fee = base_fee
+                project.cupt_code = cupt_code
             
             if items[4] != '':
                 campus = Campus.objects.get(pk=items[4])
@@ -61,9 +72,17 @@ def main():
                 round_dates.append(items[7 + rcount*2 + 1].strip())
                 
             for r in range(num_rounds):
-                project_round = AdmissionProjectRound()
+                admission_round = AdmissionRound.objects.get(pk=project_round_ids[r])
+
+                old_project_rounds = AdmissionProjectRound.objects.filter(admission_project=project,
+                                                                          admission_round=admission_round).all()
+                if len(old_project_rounds) != 0:
+                    project_round = old_project_rounds[0]
+                else:
+                    project_round = AdmissionProjectRound()
+                    
                 project_round.admission_project = project
-                project_round.admission_round = AdmissionRound.objects.get(pk=project_round_ids[r])
+                project_round.admission_round = admission_round
                 project_round.admission_dates = round_dates[r]
                 project_round.save()
 
