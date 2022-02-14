ef4c4b9f860abdae7d6cb09795c6842b3a5709ed
Jittat Fakcharoenphol <jittat@gmail.com>
2022-02-14 14:58:22 +0700

    updated import scripts

--- scripts/export_all_majors_round2.bash
+++ scripts/export_all_majors_round2.bash
@@ -3,7 +3,7 @@ declare -A PRJ
 
 BASEDIR=~/Dropbox/adm65/project-majors
 
-PRJ=( [11]=kus [12]=seed [13]=diamonds [14]=pirun [15]=thaiinter [16]=provinces [17]=sport [18]=culture [21]=irr [22]=south [23]=mou [26]=sciex [50]=inter2 )
+PRJ=( [11]=kus [12]=seed [13]=diamonds [14]=pirun [15]=thaiinter [16]=provinces [17]=sport [18]=culture [21]=irr [22]=south [23]=mou [24]=vet [26]=sciex [50]=inter2 )
 
 for num in "${!PRJ[@]}"; do
     echo $num ${PRJ[$num]}
