cc60db80fdae60bf4bf6a50848614aeb85838b0a
mameaw14 <pinyapornster@gmail.com>
2023-03-26 15:23:51 +0700

    use django form to handle form

---
+++ backoffice/forms/interview_form.py
@@ -0,0 +1,46 @@
+from django import forms
+
+
+class InterviewForm(forms.Form):
+    interview_type = forms.ChoiceField(
+        choices=[('no_interview', 'ไม่มีการสัมภาษณ์'), ('online_interview', 'สัมภาษณ์ออนไลน์'),
+                 ('on_site_interview', 'สัมภาษณ์ที่สถานที่')], widget=forms.Select(attrs={'class': 'form-control'}))
+    additional_doc = forms.ChoiceField(choices=[('no_doc', 'ไม่มี'), (
+        'has_doc', 'ต้องส่งเอกสารหรือส่งลิงก์เพิ่มเติม (กรุณาแจ้งรายละเอียดในส่วนการสัมภาษณ์ด้วย)')],
+                                       widget=forms.Select(attrs={'class': 'form-control'})
+                                       )
+    datetime = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}))
+    prep_detail = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '2'}),
+                                  required=False,
+                                  )
+    prep_img = forms.ImageField(
+        widget=forms.FileInput(attrs={'class': 'custom-file-input img-file-controls', 'data-img-id': 'prepImgId'}),
+        required=False,
+    )
+    app_type = forms.ChoiceField(
+        choices=[('webex', 'Cisco Webex'), ('zoom', 'Zoom'), ('google-meet', 'Google Meet'),
+                 ('other', 'โปรแกรมอื่น (กรุณาระบุในรายละเอียด)')],
+        widget=forms.RadioSelect,
+        required=False,
+    )
+    interview_detail = forms.CharField(
+        label='รายละเอียด',
+        help_text='ป้อนลายละเอียด เช่น ลิงก์สำหรับสัมภาษณ์ รหัสผ่าน',
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+        required=False,
+    )
+    interview_img = forms.ImageField(
+        label='รูปประกอบ',
+        help_text='รูปจะปรากฏด้านขวาของข้อความ',
+        widget=forms.FileInput(attrs={'class': 'custom-file-input img-file-controls', 'data-img-id': 'descImgId'}),
+        required=False,
+    )
+
+    # TODO: connect to the model once model is ready
+    # class Meta:
+    #     model = Interview
+    #     fields = ['interviewType', 'additionalDoc', 'datetime', 'prep_detail', 'prep_img', 'app_type']
+
+    # InterviewFormInlineFormSet = inlineformset_factory(Interview, ContactPerson, form=ContactPersonForm, extra=1,
+    #                                                    can_delete=True)
--- backoffice/views/interviews.py
+++ backoffice/views/interviews.py
@@ -1,14 +1,31 @@
 import random
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.datastructures import MultiValueDictKeyError
 
 from appl.models import Faculty, AdmissionProject, AdmissionRound
 from backoffice.decorators import user_login_required
+from backoffice.forms.contact_person_form import ContactPersonFormSet
+from backoffice.forms.interview_form import InterviewForm
 from criteria.models import MajorCuptCode, CurriculumMajor
 
 
 @user_login_required
 def interview_form(request, admission_round_id, faculty_id, description_id):
+    if request.method == 'POST':
+        form = InterviewForm(request.POST, request.FILES)
+        contact_person_formset = ContactPersonFormSet(request.POST, prefix='contact_person')
+        if form.is_valid() and contact_person_formset.is_valid():
+            print(form.cleaned_data)
+            # do something with form data, such as save to database
+            pass
+        else:
+            # print the errors to the console
+            print(form.errors)
+    else:
+        form = InterviewForm()
+        contact_person_formset = ContactPersonFormSet(prefix='contact_person')
+
     admission_round = get_object_or_404(AdmissionRound, pk=admission_round_id)
     faculty = get_object_or_404(Faculty, pk=faculty_id)
 
@@ -58,4 +75,18 @@ def interview_form(request, admission_round_id, faculty_id, description_id):
                    'admission_projects': admission_projects,
                    'majors': majors,
                    'faculty': faculty,
-                   'round_major_table': major_table})
+                   'round_major_table': major_table,
+                   'form': form,
+                   'contact_person_formset': contact_person_formset})
+
+
+def handle_file(request, name):
+    try:
+        interview_img = request.FILES[name]
+        # do something with the uploaded file
+    except MultiValueDictKeyError:
+        # no file was uploaded
+        pass
+    except Exception as e:
+        # handle other errors
+        print('Error uploading file:', e)
