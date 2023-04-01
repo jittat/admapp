6aabeb1a83e31fc1ec83470ca5d4b30b1aadc475
Monthol Charattrakool <m.charattrakool@gmail.com>
2023-04-01 23:24:06 +0700

    interview desc preview with selected project major

--- backoffice/views/interviews.py
+++ backoffice/views/interviews.py
@@ -203,27 +203,17 @@ def interview_form_preview(request, admission_round_id, faculty_id, description_
     for admission_project in admission_projects:
         admission_project.adm_rounds = set([r.id for r in admission_project.admission_rounds.all()])
 
-    selected_admission_project_major_cupt_code_list = (
-        AdmissionProjectMajorCuptCodeInterviewDescription.objects.filter(
-            admission_project__admission_rounds=admission_round_id,
-            major_cupt_code__faculty=faculty_id,
-        )
+    selected_project_major_objs = AdmissionProjectMajorCuptCodeInterviewDescription.objects.filter(
+        description_id=description_id
     )
 
-    map_selected_admission_project_major_cupt_code = dict()
-
-    for (
-            selected_admission_project_major_cupt_code
-    ) in selected_admission_project_major_cupt_code_list:
-        considered_obj_admission_project_id = (
-            selected_admission_project_major_cupt_code.admission_project.id
-        )
-        considered_obj_major_cupt_code_id = (
-            selected_admission_project_major_cupt_code.major_cupt_code.id
-        )
-        map_selected_admission_project_major_cupt_code[
-            (considered_obj_major_cupt_code_id, considered_obj_admission_project_id)
-        ] = selected_admission_project_major_cupt_code.interview_description.id
+    selected_project_major_data_list = [
+        {
+            "admission_project_titles": [obj.admission_project.title],
+            "majors": [obj.major_cupt_code.display_title()],
+        }
+        for obj in selected_project_major_objs
+    ]
 
     current_round_project_list = [
         admission_project
@@ -231,64 +221,11 @@ def interview_form_preview(request, admission_round_id, faculty_id, description_
         if admission_round.id in admission_project.adm_rounds
     ]
 
-    major_table = []
-
-    round_table = []
-    project_majors_choices = []
-    for major in majors:
-        row = []
-        for admission_project in current_round_project_list:
-            row.append(False)
-        round_table.append(row)
-
-    j = 0
-    for admission_project in current_round_project_list:
-        cmajor_set = set(
-            [
-                cm.cupt_code_id
-                for cm in curriculum_majors
-                if cm.admission_project_id == admission_project.id
-            ]
-        )
-        for i, major in enumerate(majors):
-            is_project_major_has_interview_description = (
-                                                             major.id,
-                                                             admission_project.id,
-                                                         ) in map_selected_admission_project_major_cupt_code
-
-            is_project_major_selected_by_current_form = (
-                    is_project_major_has_interview_description
-                    and map_selected_admission_project_major_cupt_code[(major.id, admission_project.id)]
-                    == description_id
-            )
-
-            is_disabled = (
-                    is_project_major_has_interview_description
-                    and not is_project_major_selected_by_current_form
-            )
-
-            project_majors_id = str(major.id) + "_" + str(admission_project.id)
-            if not is_disabled:
-                project_majors_choices.append(
-                    (project_majors_id, major.title + "_" + str(admission_project.id))
-                )
-            round_table[i][j] = {
-                "id": project_majors_id,
-                "is_disabled": is_disabled,
-                "is_checked": is_project_major_selected_by_current_form,
-                "url": "url-to-related-interview",
-            }
-
-        j += 1
-
-    major_table.extend(list(zip(majors, round_table)))
-
     if request.method == "POST":
         form = InterviewDescriptionForm(request.POST, request.FILES, instance=interview_description)
 
         form.admission_round_id = admission_round_id
         form.faculty_id = faculty_id
-        form.fields["project_majors"].choices = project_majors_choices
         if form.is_valid():
             # interview_description = form.save()
 
@@ -311,7 +248,6 @@ def interview_form_preview(request, admission_round_id, faculty_id, description_
             form = InterviewDescriptionForm(None, instance=interview_description)
         else:
             form = InterviewDescriptionForm()
-        form.fields["project_majors"].choices = project_majors_choices
 
     return render(
         request,
@@ -322,9 +258,8 @@ def interview_form_preview(request, admission_round_id, faculty_id, description_
             "admission_projects": current_round_project_list,
             "majors": majors,
             "faculty": faculty,
-            "round_major_table": major_table,
+            "selected_project_majors": selected_project_major_data_list,
             "form": form,
-            "contacts": interview_description.contacts
+            "contacts": interview_description.contacts,
         },
     )
-
