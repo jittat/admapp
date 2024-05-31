2faf3058c5cf2712e52af6c4c557a9bc6f915f8a
Jittat Fakcharoenphol <jittat@gmail.com>
2024-06-01 06:33:27 +0700

    updated criteria import

--- scripts/update_project_criteria_passed.py
+++ scripts/update_project_criteria_passed.py
@@ -6,14 +6,16 @@ import csv
 from datetime import datetime
 
 from regis.models import Applicant
-from appl.models import AdmissionProject, AdmissionResult
+from appl.models import AdmissionProject, AdmissionResult, AdmissionRound, Major, ProjectApplication
 
 
 def main():
     result_filename = sys.argv[1]
     project_id = sys.argv[2]
+    admission_round_id = sys.argv[3]
 
     admission_project = AdmissionProject.objects.get(pk=project_id)
+    admission_round = AdmissionRound.objects.get(pk=admission_round_id)
     all_applications = {}
 
     counter = 0
@@ -21,13 +23,28 @@ def main():
         reader = csv.reader(csvfile, delimiter=',')
         for items in reader:
             nat_id = items[0]
+            major_number = items[1]
+
+            major = Major.objects.filter(admission_project=admission_project, number=major_number).all()[0]
+
             applicant = Applicant.objects.get(national_id=nat_id)
-            results = AdmissionResult.objects.filter(applicant=applicant, admission_project=admission_project).all()
-            if len(results) != 1:
-                print('ERROR', nat_id, 'num results =', len(results))
-                continue
-            
-            result = results[0]
+            results = AdmissionResult.objects.filter(applicant=applicant,
+                                                     admission_project=admission_project,
+                                                     major=major).all()
+            if len(results) != 0:
+                result = results[0]
+            else:
+                applications = ProjectApplication.objects.filter(applicant=applicant,
+                                                                 admission_project=admission_project,
+                                                                 is_canceled=False).all()
+                application = applications[0]
+                result = AdmissionResult(applicant=applicant,
+                                         application=application,
+                                         admission_project=admission_project,
+                                         admission_round=admission_round,
+                                         major_rank=1,
+                                         major=major)
+                
             result.is_criteria_passed = True
             result.updated_criteria_passed_at = datetime.now()
             result.save()
