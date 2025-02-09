11836fe399b04f4c2d4c452083e0dba571092b06
Jittat Fakcharoenphol <jittat@gmail.com>
2025-02-09 16:18:49 +0700

    updated major export round2

--- scripts/export_all_majors_round2.bash
+++ scripts/export_all_majors_round2.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 declare -A PRJ
 
-BASEDIR=~/Dropbox/adm65/project-majors
+. ./export_env.bash
 
-PRJ=( [11]=kus [12]=seed [13]=diamonds [14]=pirun [15]=thaiinter [16]=provinces [17]=sport [18]=culture [21]=irr [22]=south [23]=mou [24]=vet [26]=sciex [50]=inter2 )
+BASEDIR=$PROJECT_MAJOR_BASEDIR/r2
+
+PRJ=( [11]=kus [12]=seed [13]=diamonds [14]=pirun [16]=provinces [17]=sport [18]=culture [23]=mou [24]=vet [34]=spe [35]=med [50]=inter2 )
 
 for num in "${!PRJ[@]}"; do
     echo $num ${PRJ[$num]}
