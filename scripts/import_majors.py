e96d9e6365a92aff38caaf7c7054ba58ce81d240
Jittat Fakcharoenphol <jittat@gmail.com>
2025-02-10 15:13:04 +0700

    checks title before importing majors

--- scripts/import_majors.py
+++ scripts/import_majors.py
@@ -7,16 +7,35 @@ import io
 
 from appl.models import Faculty, AdmissionProject, Major
 
+def validate_titles(lines):
+    titles = set()
+    for items in lines:
+        if len(items) < 4:
+            continue
+
+        title = (items[2].strip() + "-" + items[1].strip()).replace(" ","")
+        if title in titles:
+            print("Dupplicated title:", title)
+            return False
+        titles.add(title)
+    return True
+
 def main():
     project_id = sys.argv[1]
 
     project = AdmissionProject.objects.get(pk=project_id)
 
+    if project.major_set.count() != 0:
+        if (len(sys.argv) != 4) or (sys.argv[3] != '--remove'):
+            print("This will remove old majors.  Abort.  To actually ddo so, call with option --remove")
+            quit()
+            
     for major in project.major_set.all():
         major.delete()
     
     filename = sys.argv[2]
     counter = 0
+
     with open(filename) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         lines = [l for l in reader]
@@ -25,6 +44,10 @@ def main():
         project.column_descriptions = lines[1][0]
         project.save()
         
+        if not validate_titles(lines[2:]):
+            print("Error majors with same title")
+            quit()
+
         for items in lines[2:]:
             
             if len(items) < 4:
