df94c55ff8b19bb5e95362379d396c22406af664
Jittat Fakcharoenphol <jittat@gmail.com>
2025-04-28 19:53:54 +0700

    added export script

---
+++ scripts/export_project_applicants-r2-interview.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. ./export_env.bash
+
+for p in 11 12 13 14 16 17 18 23 24 34 35 50; do echo $p; python export_project_applicants_tcas.py $p 2 2_2568 --applicant_status > $APP_BASEDIR/r2/applicants-r2-interview-$p.csv; done
+
