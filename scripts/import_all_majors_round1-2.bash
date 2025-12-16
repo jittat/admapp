a791416b73611b26604e77b0ae74f015453b1c79
Jittat Fakcharoenphol <jittat@gmail.com>
2025-12-17 05:09:16 +0700

    updated import script (again)

--- scripts/import_all_majors_round1-2.bash
+++ scripts/import_all_majors_round1-2.bash
@@ -5,7 +5,7 @@ declare -A PRJ
 
 BASEDIR=$PROJECT_MAJOR_BASEDIR/r1-2
 
-PRJ=( [101]=ele12 [103]=inter12 [8]=dpst [109]=posn12-updated [33]=smt )
+PRJ=( [101]=ele12-updated [103]=inter12 [8]=dpst [109]=posn12-updated [33]=smt )
 
 for num in "${!PRJ[@]}"; do
     echo $num ${PRJ[$num]}
