9496d888b78dbc9ed5579e86a3c0ac1eb0e47fe9
Jittat Fakcharoenphol <jittat@gmail.com>
2026-02-14 03:51:34 +0700

    updated import script

--- scripts/import_all_majors_round2.bash
+++ scripts/import_all_majors_round2.bash
@@ -5,7 +5,7 @@ declare -A PRJ
 
 BASEDIR=$PROJECT_MAJOR_BASEDIR/r2
 
-PRJ=( [11]=kus [12]=seed [13]=diamonds [14]=pirun [16]=provinces [17]=sport [18]=culture [23]=mou [24]=vet [34]=spe [35]=med [50]=inter2 )
+PRJ=( [11]=kus [12]=seed [13]=diamonds-updated [14]=pirun-updated [16]=provinces [17]=sport [23]=mou-updated [24]=vet [34]=spe [35]=med [36]=andaman [50]=inter2 )
 
 for num in "${!PRJ[@]}"; do
     echo $num ${PRJ[$num]}
