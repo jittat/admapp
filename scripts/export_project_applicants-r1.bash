c232bdb8cf193536697529556671bae3d628b65e
Jittat Fakcharoenphol <jittat@gmail.com>
2026-02-15 20:00:26 +0700

    updated applicant export script

--- scripts/export_project_applicants-r1.bash
+++ scripts/export_project_applicants-r1.bash
@@ -2,6 +2,6 @@
 
 . ./export_env.bash
 
-for p in 1 2 3 4 8 9 32; do python export_project_applicants_tcas.py $p 1 1_2568 > $APP_BASEDIR/r1/applicants-r1-$p.csv; done
-for p in 101 103 109 33; do python export_project_applicants_tcas.py $p 5 1_2568 > $APP_BASEDIR/r1/applicants-r1-$p.csv; done
+for p in 1 2 3 4 9 18 32; do python export_project_applicants_tcas.py $p 1 1_2569 > $APP_BASEDIR/r1/applicants-r1-$p.csv; done
+for p in 101 103 8 109 33; do python export_project_applicants_tcas.py $p 5 1_2569 > $APP_BASEDIR/r1/applicants-r1-$p.csv; done
 
