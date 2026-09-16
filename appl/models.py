2eec622c76bb92738e6736fe6b54bab9334a0646
Jittat Fakcharoenphol <jittat@gmail.com>
2026-09-17 05:24:14 +0700

    updated AP message

--- appl/models.py
+++ appl/models.py
@@ -1315,7 +1315,7 @@ class Eligibility(object):
         from supplements.models import AdvancedPlacementApplicant
         self.is_eligible = False
         self.is_hidden = False
-        self.notice_text = 'โครงการนี้ผู้สมัครต้องผ่านการเข้าร่วมโครงการเรียนล่วงหน้าของม.เกษตรศาสตร์ รุ่นที่ 15-20 ปีการศึกษา 2563-2568'
+        self.notice_text = 'โครงการนี้ผู้สมัครต้องผ่านการเข้าร่วมโครงการเรียนล่วงหน้าของม.เกษตรศาสตร์ รุ่นที่ 15-21 ปีการศึกษา 2563-2569'
 
         try:
             app = AdvancedPlacementApplicant.objects.get(national_id=self._applicant.national_id)
